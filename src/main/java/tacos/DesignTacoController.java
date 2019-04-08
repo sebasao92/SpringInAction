b7bd76a335474a492b55581a2e23e9e00c1dee82
Sebastián Alarcón <sealor92@gmail.com>
2019-04-08 17:48:17 -0500

    Design view is fixed

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import tacos.Ingredient.Type;
 
 @Slf4j
@@ -18,8 +20,8 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("FLTO", "FLOUR TORTILLA", Type.WRAP),
+                new Ingredient("COTO", "CORN TORTILLA", Type.WRAP),
                 new Ingredient("GRBF", "GROUND BEEF", Type.PROTEIN),
                 new Ingredient("CARN", "CARNITAS", Type.PROTEIN),
                 new Ingredient("TMTO", "DICED TOMATOES", Type.VEGGIES),
@@ -32,11 +34,18 @@ public class DesignTacoController {
 
         Type[] types = Ingredient.Type.values();
         for(Type type : types){
-            //model.addAllAttributes(type.toString().toLowerCase(), filterByType(ingredients, type));
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
 
         model.addAttribute("design", new Taco());
 
         return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type){
+        return ingredients
+                .stream()
+                .filter(ingredient -> ingredient.getType().equals(type))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,4 +1,14 @@
 package tacos;
 
 public class Taco {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
